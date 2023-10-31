b5224274cd3efe87b51db1c9d9eadbb3dc24b5c6
Sam Komarov <sam@skomarov.com>
2023-10-31 17:23:47 +0300

    implement repo's GetAllMissing

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,12 @@
 package repository
 
 import (
+	"encoding/json"
+	"fmt"
 	"gitlab.com/samkomarov/locator-svc.git/internal/service"
 	"gitlab.com/samkomarov/locator-svc.git/internal/setup"
+	"io"
+	"net/http"
 )
 
 type ExternalAPILocatorRepo struct {
@@ -13,7 +17,21 @@ func NewExternalAPILocatorRepo(cfg setup.ExternalAPILocatorConfig) *ExternalAPIL
 	return &ExternalAPILocatorRepo{cfg}
 }
 
-func (e ExternalAPILocatorRepo) GetAllMissing() ([]service.MissingPerson, error) {
-	//TODO implement me
-	panic("implement me")
+func (e *ExternalAPILocatorRepo) GetAllMissing() ([]service.MissingPerson, error) {
+	resp, err := http.Get(e.cfg.EndpointURL)
+	if err != nil {
+		return nil, fmt.Errorf("while making request to external api: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("making request to external api returned %d: %s", resp.StatusCode, body)
+	}
+	var body struct {
+		Result []service.MissingPerson `json:"result"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&body)
+	if err != nil {
+		return nil, fmt.Errorf("while decoding external api's response body: %v", err)
+	}
+	return body.Result, nil
 }
