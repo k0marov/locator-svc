b2461aac3afc4dba6334211bd213ab901470fc3c
Sam Komarov <sam@skomarov.com>
2023-11-01 18:49:28 +0300

    refactor url query parsing

--- internal/delivery/server.go
+++ internal/delivery/server.go
@@ -2,9 +2,11 @@ package delivery
 
 import (
 	"encoding/json"
+	"fmt"
 	"gitlab.com/samkomarov/locator-svc.git/internal/core"
 	"gitlab.com/samkomarov/locator-svc.git/internal/service"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 import "github.com/go-chi/chi/v5"
@@ -32,20 +34,14 @@ func (s *Server) defineEndpoints() {
 
 func (s *Server) GetMissing(w http.ResponseWriter, r *http.Request) {
 	urlQuery := r.URL.Query()
-	latitude, err := strconv.ParseFloat(urlQuery.Get("latitude"), 64)
+	latitude, err := parseFloatQuery(urlQuery, "latitude")
 	if err != nil {
-		core.WriteErrorResponse(w, &core.ClientError{
-			DisplayMessage: "couldn't parse 'latitude' url parameter as float",
-			HTTPCode:       http.StatusBadRequest,
-		})
+		core.WriteErrorResponse(w, err)
 		return
 	}
-	longitude, err := strconv.ParseFloat(urlQuery.Get("longitude"), 64)
+	longitude, err := parseFloatQuery(urlQuery, "longitude")
 	if err != nil {
-		core.WriteErrorResponse(w, &core.ClientError{
-			DisplayMessage: "couldn't parse 'longitude' url parameter as float",
-			HTTPCode:       http.StatusBadRequest,
-		})
+		core.WriteErrorResponse(w, err)
 		return
 	}
 
@@ -57,16 +53,20 @@ func (s *Server) GetMissing(w http.ResponseWriter, r *http.Request) {
 		core.WriteErrorResponse(w, err)
 		return
 	}
-	err = json.NewEncoder(w).Encode(missing)
-	if err != nil {
-		core.WriteErrorResponse(w, &core.ClientError{
-			DisplayMessage: err.Error(),
-			HTTPCode:       http.StatusBadRequest,
-		})
-		return
-	}
+	json.NewEncoder(w).Encode(missing)
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.r.ServeHTTP(w, r)
+}
+
+func parseFloatQuery(query url.Values, name string) (float64, error) {
+	value, err := strconv.ParseFloat(query.Get(name), 64)
+	if err != nil {
+		return 0, &core.ClientError{
+			DisplayMessage: fmt.Sprintf("couldn't parse '%s' url parameter as float", name),
+			HTTPCode:       http.StatusBadRequest,
+		}
+	}
+	return value, nil
 }
